54aa48adf10eb2529a37ad099d057b065e4ffdfc
sejoung kim <sejoung@gmail.com>
2021-02-03 18:38:10 +0900

    a

---
+++ EffectiveCSharp/Item19/ReverseEnumerable.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace EffectiveCSharp.Item19
+{
+	public sealed class ReverseEnumerable<T> : IEnumerable<T>
+	{
+		private class ReverseEnumerator : IEnumerator<T>
+		{
+			int currentIndex;
+			IList<T> collection;
+			public ReverseEnumerator(IList<T> srcCollection)
+			{
+				currentIndex = srcCollection.Count;
+				collection = srcCollection;
+			}
+			public T Current => collection[currentIndex];
+
+			object IEnumerator.Current => this.Current;
+
+			public void Dispose()
+			{
+
+			}
+
+			public bool MoveNext()
+			{
+				return --currentIndex >= 0;
+			}
+
+			public void Reset()
+			{
+				currentIndex = collection.Count;
+			}
+		}
+		IEnumerable<T> sourceSequence;
+		IList<T> originalSequence;
+		public ReverseEnumerable(IEnumerable<T> sequence)
+		{
+			sourceSequence = sequence;
+		}
+		public ReverseEnumerable(IList<T> sequence)
+		{
+			sourceSequence = sequence;
+			originalSequence = sequence;
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			if (originalSequence == null)
+			{
+				if (sourceSequence is Collection<T>)
+				{
+					ICollection<T> source = sourceSequence as ICollection<T>;
+					originalSequence = new List<T>(source.Count);
+				}
+				else
+				{
+					originalSequence = new List<T>();
+				}
+				foreach (T item in sourceSequence)
+				{
+					originalSequence.Add(item);
+				}
+			}
+			return new ReverseEnumerator(originalSequence);
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return this.GetEnumerator();
+		}
+	}
+}
---
+++ EffectiveCSharp/Item20/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EffectiveCSharp.Item20
+{
+	public struct Customer : IComparable<Customer>, IComparable
+	{
+		private readonly string name;
+
+		public Customer(string name)
+		{
+			this.name = name;
+		}
+
+		public int CompareTo(Customer other) => string.Compare(name, other.name, StringComparison.Ordinal);
+
+		public int CompareTo(object obj)
+		{
+			if (!(obj is Customer))
+			{
+				throw new ArgumentException("argument is not Customer");
+			}
+
+			var otherCustomer = (Customer) obj;
+
+			return CompareTo(otherCustomer);
+		}
+
+		public static bool operator <(Customer left, Customer right) => left.CompareTo(right) < 0;
+		public static bool operator >(Customer left, Customer right) => left.CompareTo(right) > 0;
+		public static bool operator <=(Customer left, Customer right) => left.CompareTo(right) <= 0;
+		public static bool operator >=(Customer left, Customer right) => left.CompareTo(right) >= 0;
+	}
+}
